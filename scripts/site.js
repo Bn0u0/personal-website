926b9a79ca8a6dd63f034e35aee5b8cd1b90d38f
Bn0u0 <alan007.0003@gmail.com>
2026-08-22 02:50:41 +0800

    perf: speed up project detail closing

--- scripts/site.js
+++ scripts/site.js
@@ -141,7 +141,7 @@
 
     const from=detailRadius;
     const to=opening?detailMaxRadius:2;
-    const duration=1500;
+    const duration=opening?1500:1000;
     const started=performance.now();
 
     const frame=now=>{
